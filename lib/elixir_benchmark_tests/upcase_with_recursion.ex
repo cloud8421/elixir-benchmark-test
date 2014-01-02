f8415b571aec37075d1e6820ac7d8531c5d69a2e
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-01-02 17:27:55 +0100

    Fixes upcase test case and adds tail recursion implementation.

--- lib/elixir_benchmark_tests/upcase_with_recursion.ex
+++ lib/elixir_benchmark_tests/upcase_with_recursion.ex
@@ -5,4 +5,12 @@ defmodule ElixirBenchmarkTests.UpcaseWithRecursion do
     [ String.upcase(head) | upcase(tail) ]
   end
 
+  def tail_upcase(word_list) do
+    tail_upcase(word_list, [])
+  end
+  defp tail_upcase([], acc), do: acc |> Enum.reverse
+  defp tail_upcase([element | tail], acc) do
+    tail_upcase(tail, [ String.upcase(element) | acc ])
+  end
+
 end
--- test/map_vs_recursion_test.exs
+++ test/map_vs_recursion_test.exs
@@ -9,12 +9,18 @@ defmodule MapVsRecursion do
     IO.inspect { :upcase_with_recursion, average, total }
   end
 
+  test "upcase with tail recursion" do
+    [_min, _max, average, total, _reps] = Benchmark.times 100000 do
+      ElixirBenchmarkTests.UpcaseWithRecursion.tail_upcase(["foo", "bar", "baz"])
+    end
+    IO.inspect { :upcase_with_tail_recursion, average, total }
+  end
+
   test "upcase with map" do
     [_min, _max, average, total, _reps] = Benchmark.times 100000 do
       ElixirBenchmarkTests.UpcaseWithMap.upcase(["foo", "bar", "baz"])
     end
     IO.inspect { :upcase_with_map, average, total }
   end
-
 
 end
